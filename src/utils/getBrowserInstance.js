7c6f87bff80857ec6216607985481d9c5ad69f82
Greg Hesp <greg.hesp@gmail.com>
2021-12-24 17:10:33 +0000

    Use Puppeteer to get Wakefield working

--- src/utils/getBrowserInstance.js
+++ src/utils/getBrowserInstance.js
@@ -1,24 +1,9 @@
 export default async function () {
-  const executablePath = await chromium.executablePath;
-
-  if (!executablePath) {
-    // running locally
-    const puppeteer = require("puppeteer");
-    return puppeteer.launch({
-      headless: true,
-      defaultViewport: null,
-      args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote"],
-    });
-  }
-
-  return chromium.puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: {
-      width: 1280,
-      height: 720,
-    },
-    executablePath,
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
+  // running locally
+  const puppeteer = require("puppeteer");
+  return puppeteer.launch({
+    headless: true,
+    defaultViewport: null,
+    args: ["--incognito", "--no-sandbox", "--single-process", "--no-zygote"],
   });
 }
